61b61e5e0ff9cc2f8626d210084b03d6cd04ccc1
Prajwal Shreshta Grandhi <70463518+PrajwalGrandhi@users.noreply.github.com>
2021-10-17 14:16:34 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,36 +2,33 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 import csv
+import requests
+import pandas as pd
 
 START_URL='https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
-browser=webdriver.Chrome("chromedriver.exe")
-browser.get(START_URL)
 time.sleep(10)
 
-def scrape():
-    headers=['name','distance','mass','radius']
-    stars=[]
-    for i in range(0,98):
-        soup=BeautifulSoup(browser.page_source,"html.parser")
-        for tbody in soup.find_all("tbody"):
-            tr_tags=tbody.find_all("tr")
-            td_tags=tr_tags.find("td")
+star_names=[]
+distance=[]
+mass=[]
+radius=[]
+lum=[]
+temp_list=[]
+page=requests.get(START_URL)
+soup=BeautifulSoup(page.text,"html.parser")
+star_table=soup.find("table")
+table_rows=star_table.find_all("tr")
+for tr in table_rows:
+    td=tr.find_all("td")
+    row=[i.text.rstrip() for i in td]
+    temp_list.append(row)
 
-            temp_list=[]
-            for ind,td_tag in enumerate(td_tags):
-                if(ind==0):
-                    temp_list.append(td_tag.find_all("a")[0].contents[0])
-                else:
-                    try:
-                        temp_list.append(td_tag.contents[0])
-                    except:
-                        temp_list.append("")
-            stars.append(temp_list)   
-    with open("scraped.csv","w") as f:
-        csv_writer=csv.writer(f)
-        csv_writer.writerow(headers)
-        csv_writer.writerows(stars)
-    
-scrape()
+for i in range(1,len(temp_list)):
+    star_names.append(temp_list[i][1])
+    distance.append(temp_list[i][3])
+    mass.append(temp_list[i][5])
+    radius.append(temp_list[i][6])
+    lum.append(temp_list[i][7])
 
-
+df2=pd.DataFrame(list(zip(star_names,distance,mass,radius,lum)),columns=['name','distance','mass','radius',"luminosity"])
+df2.to_csv("scraped.csv")
